74b398bc9691f4857f22d06dda01b48578f17e61
vladislav1137 <mr.vladislav220@yandex.ru>
2023-02-18 21:32:55 +0300

    commit 1

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,9 @@ public class Main {
 
     public static void task1 () {
         System.out.println("Задача 1");
+        for (int i = 1; i < 11; i++){
+            System.out.println(i);
+        }
     }
 
     public static void task2 () {
