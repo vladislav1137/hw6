da3e093d3830fa2e0c82ca687f90091b9707c1e0
vladislav1137 <mr.vladislav220@yandex.ru>
2023-02-18 21:41:14 +0300

    commit 5

--- src/Main.java
+++ src/Main.java
@@ -40,6 +40,9 @@ public class Main {
     }
     public static void task5 () {
         System.out.println("Задача 5");
+        for (int i = 1904; i <2096; i = i +4) {
+            System.out.println(i + " год является високосным ");
+        }
     }
 
     public static void task6 () {
