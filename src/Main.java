d29abdeb052994be6b7a9c89c89326dc702493e0
vladislav1137 <mr.vladislav220@yandex.ru>
2023-02-18 22:10:47 +0300

    commit 9

--- src/Main.java
+++ src/Main.java
@@ -70,6 +70,13 @@ public class Main {
     }
     public static void task9 () {
         System.out.println("Задача 9");
+        int inMonth= 29000;
+        int total = 0;
+        for (int i = 1; i<13; i++){
+            total = total + total/100;
+            total = total + inMonth;
+            System.out.println("Месяц " + i +", сумма накоплений равна " + total+  " рублей");
+        }
     }
 
     public static void task10 () {
